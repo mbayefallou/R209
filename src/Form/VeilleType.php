d7e1066a1d0d01760ee42791e0141d4e6b0307f3
Your Name <you@example.com>
2024-05-31 11:25:07 +0200

    changement

--- src/Form/VeilleType.php
+++ src/Form/VeilleType.php
@@ -6,14 +6,17 @@ use App\Entity\Veille;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 class VeilleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('nom')
-            ->add('dateD')
+            ->add('dateD',DateType::class, [
+                'widget' => 'single_text',
+                'label' => "Date d'aquisition"
+                ])
             ->add('acquisition')
             ->add('veilleContinue')
             ->add('category')
